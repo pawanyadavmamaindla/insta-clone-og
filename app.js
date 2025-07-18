a518649de85063a11f5494d840b7df6dc5ca45f0
MAMINDLA PAWAN YADAV <145900443+pawanyadavmamaindla@users.noreply.github.com>
2025-07-18 19:21:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,13 +28,12 @@ app.use(require('./routes/user'))
 app.get('/',(req,res)=>{
     res.send("hello")
 })
-// if(process.env.NODE_ENV=="production"){
-//     app.use(express.static('client/build'))
-//     const path = require('path');
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+    app.use(express.static('client/build'))
+    const path = require('path');
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
